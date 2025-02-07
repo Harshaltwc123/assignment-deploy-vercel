d662c24fd5bb1f674da1312e7370cc4c614928e8
Harshaltwc123 <harshaltwc004@gmail.com>
2025-02-07 08:03:42 +0530

    add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-purple-900 2xl:bg-red-500 xl:bg-green-200 lg:bg-yellow-300 md:bg-purple-500 sm:bg-blue-200">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
