6e109d35a0444cff1d0bc198c70259bb2cd3cdcd
Harshaltwc123 <harshaltwc004@gmail.com>
2025-02-09 21:26:03 +0530

    add Invest

--- src/component/Invest.jsx
+++ src/component/Invest.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import img1 from "../assets/Invest1.png";
 import img2 from "../assets/Invest2.png";
 import img3 from "../assets/Invest3.png";
+import Rating from "../assets/Rating.png";
+import Testimonials1 from "../assets/Testimonials1.png";
+import Testimonials2 from "../assets/Testimonials2.png";
+import Testimonials3 from "../assets/Testimonials3.png";
 
 function Invest() {
   return (
@@ -55,6 +59,83 @@ function Invest() {
               Hubnex Invest's innovative technologies have garnered industry
               recognition and are paving the way for a greener future.
             </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Testimonials */}
+      <div className="bg-black text-white py-10">
+        <h1 className="font-semibold text-3xl pb-12">Testimonials</h1>
+        <div className="flex flex-wrap justify-evenly items-center gap-4">
+          {/* card1 */}
+          <div className="bg-zinc-800 max-w-xs p-4 rounded-xl">
+            <div className="flex justify-end pb-2">
+              <img src={Rating} alt="" className="h-3" />
+            </div>
+            <p className="text-sm">
+              “Partnering with Hubnex Invest was a game-changer for our startup.
+              Their network, resources, and investment have accelerated our
+              growth and opened doors to new opportunities. We're proud to be
+              part of the Hubnex Invest community and grateful for their
+              unwavering support."
+            </p>
+            <div className="flex justify-evenly items-center pt-4 pr-16">
+              <img
+                src={Testimonials1}
+                alt=""
+                className="rounded-full w-20 h-20"
+              />
+              <ul className="flex flex-col gap-2 justify-start items-start">
+                <li>Verma S</li>
+                <li className="text-xs">Founder C, Startup Z</li>
+              </ul>
+            </div>
+          </div>
+          {/* card2 */}
+          <div className="bg-zinc-800 max-w-xs p-4 rounded-xl">
+            <div className="flex justify-end pb-2">
+              <img src={Rating} alt="" className="h-3" />
+            </div>
+            <p className="text-sm">
+              “Hubnex Invest’s support was instrumental in turning our vision
+              into reality. Their guidance, mentorship,and investment empowered
+              us to overcome challenges and achieve our goals. We’re grateful
+              for their partnership and support on our Journey to success.”
+            </p>
+            <div className="flex justify-evenly items-center pt-8 pr-16">
+              <img
+                src={Testimonials2}
+                alt=""
+                className="rounded-full w-20 h-20"
+              />
+              <ul className="flex flex-col gap-2 justify-start items-start">
+                <li>Jayasree M</li>
+                <li className="text-xs">Founder A, Startup X</li>
+              </ul>
+            </div>
+          </div>
+          {/* card3 */}
+          <div className="bg-zinc-800 max-w-xs p-4 rounded-xl">
+            <div className="flex justify-end pb-2">
+              <img src={Rating} alt="" className="h-3" />
+            </div>
+            <p className="text-sm">
+              “As an investor, I’ve been impressed by the caliber of startups
+              supported by Hubnex Invest. Their rigorous selection process and
+              strategic support have resulted in a portfolio of high potential
+              ventures with compelling growth prospects.”
+            </p>
+            <div className="flex justify-evenly items-center pt-8 pr-16">
+              <img
+                src={Testimonials3}
+                alt=""
+                className="rounded-full w-20 h-20"
+              />
+              <ul className="flex flex-col gap-2 justify-start items-start">
+                <li>Anirudh  H</li>
+                <li className="text-xs">Founder B, Startup  Y</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
